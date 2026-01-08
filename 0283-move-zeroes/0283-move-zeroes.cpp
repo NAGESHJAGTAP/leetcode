52b258278493bb6e61a249c125ff66728d579600
Nagesh jagtap <nageshjagtap063@gmail.com>
2026-01-08 22:17:02 +0530

    Time: 0 ms (100%), Space: 23.8 MB (54.11%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        // int pos=0;
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i] != 0){
+        //      nums[pos]=nums[i];
+        //      pos++;
+        //     }
+        // }
+        // for(int i=pos; i<nums.size(); i++){
+        //     nums[i] = 0;
+        // }
+
+
+
+
+        // int pos=0;
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i] != 0){
+        //         swap(nums[pos], nums[i]);
+        //         pos++;
+        //     }
+        // }
+
+
+
+     
+
+     int k=0;
+     for(int i=0; i<nums.size(); i++){
+        if(nums[i] != 0){
+          nums[k]=nums[i];
+           k++;
+        }
+     }
+     for(int i=k; i<nums.size(); i++){
+        nums[i]=0;
+     }
+
+    }
+};
