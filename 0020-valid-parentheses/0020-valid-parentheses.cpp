b70301bb7993c80ccd0391d39b0d64623fd83b75
Nagesh jagtap <nageshjagtap063@gmail.com>
2026-01-17 11:01:10 +0530

    Time: 0 ms (100%), Space: 10.1 MB (5.66%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0; i<s.size(); i++){
+            int c=s[i];
+          if (c == '(' || c == '{' || c == '['){
+                 st.push(c);
+            }else{
+               if(st.empty()) return false;
+
+             if ((c == ')' && st.top() != '(') ||
+                (c == '}' && st.top() != '{') ||
+                (c == ']' && st.top() != '['))
+               return false;
+
+               st.pop();
+            }
+        }
+           return st.empty();
+    }
+};
