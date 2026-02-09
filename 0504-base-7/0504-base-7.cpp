bfae370073fd39ab1e1cd57bfb8c153c6e6f0c36
Nagesh jagtap <nageshjagtap063@gmail.com>
2026-02-09 23:46:58 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.39%) - LeetHub

---
+++ 0504-base-7/0504-base-7.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if(num==0) return "0";
+        bool negative=num<0;
+        num=abs(num);
+        string result="";
+        while(num>0){
+            result=char('0' + (num%7))+result;
+            num/=7;
+        }
+
+        if (negative) result="-"+result;
+        return result;
+    }
+};
