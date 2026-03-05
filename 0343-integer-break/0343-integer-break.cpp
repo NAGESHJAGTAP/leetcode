b1b3fcae4c188ea8fc5a69c75d299f60318c123e
Nagesh jagtap <nageshjagtap063@gmail.com>
2026-03-05 18:55:48 +0530

    Time: 0 ms (100%), Space: 7.9 MB (74.68%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n==2) return 1;
+        if(n==3) return 2;
+
+        int maxProduct = 1;
+
+        while(n > 4) {
+            maxProduct *= 3;
+            n -= 3;
+        }
+        return maxProduct * n;
+    }
+};
