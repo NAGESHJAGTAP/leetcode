19cfb8584e2d33a15ac5101a28166c4afaff2931
Nagesh jagtap <nageshjagtap063@gmail.com>
2026-03-30 23:48:28 +0530

    Time: 0 ms (100%), Space: 8.9 MB (66.94%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -3,8 +3,8 @@ public:
     int lengthOfLastWord(string s) {
         int count=0;
         for(int i=s.size()-1; i>=0; i--){
-            if(s[i]==' ' && count>0){
-                 break;
+            if(s[i] == ' ' && count>0){
+                break;
             }
             if(s[i] != ' '){
                 count++;
