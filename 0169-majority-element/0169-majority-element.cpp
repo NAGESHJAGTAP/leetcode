2ec9103944efeeb57d179b252e9f504828cb5ae4
Nagesh jagtap <nageshjagtap063@gmail.com>
2026-08-18 00:25:55 +0530

    Time: 0 ms (100%), Space: 28.2 MB (44%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,27 +1,20 @@
-// class Solution {
-// public:
-//     int majorityElement(vector<int>& nums) {
-//         int max=0; 
-//         int count=0;
-//         for(int i=0; i<nums.size(); i++){
-//             if(count==0){
-//                 max=nums[i];
-//             }
-//             if(nums[i]==max){
-//                 count++;
-//             }else{
-//                 count--;
-//             }
-//         }
-//         return max;
-//     }
-// };
-
-
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size() / 2];
+        int count=0;
+        int max=0;
+
+        for(int i=0; i<nums.size(); i++){
+            if(count == 0){
+                max=nums[i];
+            }
+
+            if(nums[i] == max){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return max;
     }
 };
